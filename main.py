eb128654e65516abfead32e5eef0fb24fc88d736
Jan Warchocki <janwarchocki@gmail.com>
2024-05-29 00:00:17 +0200

    Generalized the setup, reformatted some code

--- main.py
+++ main.py
@@ -6,7 +6,13 @@ import FitnessFunction
 
 if __name__ == "__main__":
     # "CustomCrossover", "UniformCrossover", "OnePointCrossover"
-    crossovers = ["TwoPointCrossover","GreedyCrossover","GreedyMutCrossover", "UniformCrossover", "OnePointCrossover"]
+    setups = [
+        {'variation': 'TwoPointCrossover'},
+        {'variation': 'GreedyCrossover'},
+        {'variation': 'GreedyMutCrossover'},
+        {'variation': 'UniformCrossover'},
+        {'variation': 'OnePointCrossover'},
+    ]
     evaluation_dictionary = {}
     evaluation_budget = 100000
     population_size = 1000
@@ -16,26 +22,38 @@ if __name__ == "__main__":
         print(f"Running: {set_name}: {instance_names}, with {vertex_amount} vertices")
         for instance_name in instance_names:
             instance_path = f"maxcut-instances/{set_name}/{instance_name}"
-            for cx in crossovers:
-                with open("output-{}.txt".format(cx),"w") as f:
+            for setup in setups:
+                with open("output-{}.txt".format(setup['variation']),"w") as f:
                     num_evaluations_list = []
                     num_runs = 10
                     num_success = 0
                     runs = []
                     for i in range(num_runs):
                         fitness = FitnessFunction.MaxCut(instance_path)
-                        genetic_algorithm = GeneticAlgorithm(fitness,population_size,variation=cx,evaluation_budget=evaluation_budget,verbose=False, save_stats=True)
+                        genetic_algorithm = GeneticAlgorithm(fitness,
+                                                             population_size,
+                                                             evaluation_budget=evaluation_budget,
+                                                             verbose=False,
+                                                             save_stats=True,
+                                                             **setup)
 
                         best_fitness, num_evaluations = genetic_algorithm.run()
                         runs.append(genetic_algorithm.statistics)
                         if best_fitness == fitness.value_to_reach:
                             num_success += 1
                         num_evaluations_list.append(num_evaluations)
-                    plot_runs_per_generation(runs, [f"{i}" for i in range(len(runs))], StatType.BEST_FITNESS, f"Best fitness per generation across runs for {cx}\nWith {num_success}/{num_runs} reaching the optimum", set_name, f"{vertex_amount}_{instance_name}")
+                    plot_runs_per_generation(runs,
+                                             [f"{i}" for i in range(len(runs))],
+                                             StatType.BEST_FITNESS,
+                                             f"Best fitness per generation across runs for {setup['variation']}\nWith {num_success}/{num_runs} reaching the optimum",
+                                             set_name,
+                                             f"{vertex_amount}_{instance_name}")
 
-                    evaluation_dictionary[cx] = num_evaluations_list
+                    evaluation_dictionary[setup['variation']] = num_evaluations_list
                     print("{}/{} runs successful".format(num_success,num_runs))
                     print("{} evaluations (median)".format(np.median(num_evaluations_list)))
                     percentiles = np.percentile(num_evaluations_list,[10,50,90])
                     f.write("{} {} {} {} {}\n".format(population_size,num_success/num_runs,percentiles[0],percentiles[1],percentiles[2]))
+
+            crossovers = [setup['variation'] for setup in setups]
             plot_evaluation_for_crossovers(evaluation_dictionary, crossovers, population_size, evaluation_budget, (vertex_amount, set_name, instance_name))
